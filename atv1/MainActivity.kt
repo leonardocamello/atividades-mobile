7a563995db3f045ac53bc2fa574b0378a7f3b916
Leonardo Camello <jlcvo@discente.ifpe.edu.br>
2026-07-03 13:54:41 -0300

    Create MainActivity.kt

---
+++ atv1/MainActivity.kt
@@ -0,0 +1,95 @@
+package com.app.primeiroprojeto
+
+import android.os.Bundle
+import android.os.Message
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.app.primeiroprojeto.ui.theme.PrimeiroProjetoTheme
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.ui.res.painterResource
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PrimeiroProjetoTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MessageCard()
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+data class  Message(val author:string, val body:string)
+@Composable
+
+fun MessageCard() {
+    Row {
+        Image(
+            painter = painterResource(R.drawable.imagemif),
+            contentDescription = "Contact profile picture",
+        )
+
+        Column {
+            Text(text = msg.author)
+            Text(text = msg.body)
+        }
+
+    }
+
+}
+
+
+@Preview(showBackground = true)
+@Composable
+
+fun MessageCard(msg: Message) {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.imagemif),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
+}
